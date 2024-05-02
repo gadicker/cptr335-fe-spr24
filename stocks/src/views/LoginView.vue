4f638118be008727c7e28302543d2b964c1eaa44
Denton Underwood <denton.underwood@gmail.com>
2024-05-02 08:43:12 -0500

    Wrapped the username, password, and button in a form tag that calls the authenticate function on Enter Key press as well as button click.

--- stocks/src/views/LoginView.vue
+++ stocks/src/views/LoginView.vue
@@ -106,45 +106,45 @@ function submitForm(email, password) {
           <form class="login100-form validate-form">
             <span class="login100-form-title"> Member Login </span>
 
-            <div
-              class="wrap-input100 validate-input"
-              data-validate="Valid email is required: ex@abc.xyz"
-            >
-              <input
-                class="input100"
-                type="text"
-                name="email"
-                placeholder="Email"
-                v-model="email"
-              />
-              <span class="focus-input100"></span>
-              <span class="symbol-input100">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-              </span>
-            </div>
+            <form @submit.prevent="authenticate">
+              <div
+                class="wrap-input100 validate-input"
+                data-validate="Valid email is required: ex@abc.xyz"
+              >
+                <input
+                  class="input100"
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  v-model="email"
+                />
+                <span class="focus-input100"></span>
+                <span class="symbol-input100">
+                  <i class="fa fa-envelope" aria-hidden="true"></i>
+                </span>
+              </div>
 
-            <div class="wrap-input100 validate-input" data-validate="Password is required">
-              <input
-                class="input100"
-                type="password"
-                name="pass"
-                placeholder="Password"
-                v-model="password"
-              />
-              <span class="focus-input100"></span>
-              <span class="symbol-input100">
-                <i class="fa fa-lock" aria-hidden="true"></i>
-              </span>
-            </div>
+              <div class="wrap-input100 validate-input" data-validate="Password is required">
+                <input
+                  class="input100"
+                  type="password"
+                  name="pass"
+                  placeholder="Password"
+                  v-model="password"
+                />
+                <span class="focus-input100"></span>
+                <span class="symbol-input100">
+                  <i class="fa fa-lock" aria-hidden="true"></i>
+                </span>
+              </div>
 
-            <div class="container-login100-form-btn">
-              <span class="login100-form-btn" @click="authenticate">
-                <a class="txt2">
-                  Login
+              <div class="container-login100-form-btn">
+                <button type="submit" class="login100-form-btn">
+                  <span>Login</span>
                   <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-                </a>
-              </span>
-            </div>
+                </button>
+              </div>
+            </form>
 
             <div class="text-center p-t-12">
               <span class="txt1"> Forgot </span>
